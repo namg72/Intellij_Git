1502772bc4808626a6bff4e103de1c3023190b02
Nacho <namg72@gmail.com>
2022-02-03 19:01:38 +0100

    comienzo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int num1=2;
+        int num2=3;
+
+        int suma=num1 + num2;
+
+        System.out.println(suma);
+    }
+}
